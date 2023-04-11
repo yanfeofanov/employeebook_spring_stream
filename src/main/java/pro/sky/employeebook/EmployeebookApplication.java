005d2ee323492597d3e61476abb1811fca56bbed
kaVII-x <ka52jiguli@yandex.ru>
2023-04-11 18:41:51 +0200

    Создание pull

---
+++ src/main/java/pro/sky/employeebook/EmployeebookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeebookApplication.class, args);
+	}
+
+}
